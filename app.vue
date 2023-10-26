021bf1df5393b53018256609302065401f00d7e6
brandon <brandoge91@gmail.com>
2023-10-26 13:48:22 -0400

    fix discor duser

--- app.vue
+++ app.vue
@@ -15,7 +15,7 @@
           <div>
             <div class="flex justify-center underline">
               <Icon name="ic:baseline-discord" class="mt-0.5" size="24" />
-              <a>   @brandonap.</a>
+              <a>   @brandonapt</a>
             </div>
             <div class="flex justify-center underline">
               <Icon name="mdi:github" class="mt-0.5" size="24" />
